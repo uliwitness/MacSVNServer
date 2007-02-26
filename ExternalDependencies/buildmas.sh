6270b65fdefe49d73e2c593ff892a481e3e44648
kroc_camen <kroc_camen@3a4a594a-022a-0410-b5d1-ff16018dbe41>
2007-02-26 00:06:50 +0000

    kroc: build script now checks that the XCode project has already been built first before running the script. Also checks that the archives are present before unpacking them. Script stops if any critical errors occur in the build processes. This turned up an error in gettext that wouldn't build. This was fixed by downloading gettext-0.16.1.tar.gz (over 0.15). The ExternalDependencies.zip therefore needs to be updated. * Things seemed to be half finished. I noticed that the neon, apr, and apr-util archives were not being extracted. I've added the code to extarct these, and fixed the symlinks section to correctly symlink the extracted folders into the subversion folder to get it to build correctly. * I've made ./configure and make commands quieter so that a lot less is thrown out to screen. This makes it easier to tell at what stage you are at, and makes things more legible. I might extend the script to enable/disable this behaviour with an argument at a later date.
    git-svn-id: svn://svn.code.sf.net/p/macsvnserver/code@7 3a4a594a-022a-0410-b5d1-ff16018dbe41

--- ExternalDependencies/buildmas.sh
+++ ExternalDependencies/buildmas.sh
@@ -10,8 +10,18 @@ die()
 WORKING_DIR="`dirname $0`"	# This may give a relative path.
 cd "$WORKING_DIR"
 WORKING_DIR="`pwd`"		# pwd always gives absolute paths.
+
+# get the location of the application bundle outputted by the XCode build
 MAS_PREFIX="$WORKING_DIR/../Application/build/Release/MAS.app/Contents/Resources/MAS"
 SOURCES_DIR="`echo $WORKING_DIR`/Sources/"
+
+# has the user built the XCode project first?
+if ! [ -d "$WORKING_DIR/../Application/build/Release/MAS.app" ]
+then
+	echo "! the XCode project must be built first"
+	echo "  please open 'macsvnserver/Application/MAS.xcodeproj' and click the Build button"
+	exit 1
+fi
 
 # Have a sources folder? Use it, otherwise extract stuff from
 # the Archives folder into a new Sources folder:
@@ -20,57 +30,102 @@ then
     echo "Using existing Sources folder."
     cd $SOURCES_DIR
 else
+	echo "############################################################"
+	echo "# UNPACKING SOURCES"
+	echo ""
+	
+	ISERROR=false
+	#!## locate expat source code archive
+	#!#EXPAT_ARCHIVE="`find Archives -name 'expat-*.tar.gz' -print -maxdepth 1 2>/dev/null`"
+	#!#if ! [ -n "$EXPAT_ARCHIVE" ]; then echo "! expat source code archive is missing"; echo "  expecting 'macsvnserver/ExternalDependencies/Archives/expat-?.?.?.tar.gz'"; ISERROR=true; fi
+	# locate libxml source code archive
+	LIBXML_ARCHIVE="`find Archives -name 'libxml2-*.tar.gz' -print -maxdepth 1 2>/dev/null`"
+	if ! [ -n "$LIBXML_ARCHIVE" ]; then echo "! libxml source code archive is missing"; echo "  expecting 'macsvnserver/ExternalDependencies/Archives/libxml2-?.?.?.tar.gz'"; ISERROR=true; fi
+	# locate gettext source code archive
+	GETTEXT_ARCHIVE="`find Archives -name 'gettext-*.tar.gz' -print -maxdepth 1 2>/dev/null`"
+	if ! [ -n "$GETTEXT_ARCHIVE" ]; then echo "! gettext source code archive is missing"; echo "  expecting 'macsvnserver/ExternalDependencies/Archives/gettext-?.?.?.tar.gz'"; ISERROR=true; fi
+	# locate neon source code archive
+	NEON_ARCHIVE="`find Archives -name 'neon-*.tar.gz' -print -maxdepth 1 2>/dev/null`"
+	if ! [ -n "$NEON_ARCHIVE" ]; then echo "! neon source code archive is missing"; echo "  expecting 'macsvnserver/ExternalDependencies/Archives/neon-?.?.?.tar.gz'"; ISERROR=true; fi
+	# locate apr source code archive
+	APR_ARCHIVE="`find Archives -regex '.*/apr-[^u]*\.tar\.gz' -print -maxdepth 1 2>/dev/null`"
+	if ! [ -n "$APR_ARCHIVE" ]; then echo "! apr source code archive is missing"; echo "  expecting 'macsvnserver/ExternalDependencies/Archives/apr-?.?.?.tar.gz'"; ISERROR=true; fi
+	# locate apr-util source code archive
+	APRUTIL_ARCHIVE="`find Archives -regex '.*/apr-util-.*\.tar\.gz' -print -maxdepth 1 2>/dev/null`"
+	if ! [ -n "$APRUTIL_ARCHIVE" ]; then echo "! apr-util source code archive is missing"; echo "  expecting 'macsvnserver/ExternalDependencies/Archives/apr-util-?.?.?.tar.gz'"; ISERROR=true; fi
+	# locate apache source code archive
+	HTTPD_ARCHIVE="`find Archives -name 'httpd-*.tar.gz' -print -maxdepth 1 2>/dev/null`"
+	if ! [ -n "$HTTPD_ARCHIVE" ]; then echo "! Apache source code archive is missing"; echo "  expecting 'macsvnserver/ExternalDependencies/Archives/httpd-?.?.?.tar.gz'"; ISERROR=true; fi
+	# locate SubVersioN source code archive
+	SVN_ARCHIVE="`find Archives -name 'subversion-*.tar.gz' -print -maxdepth 1 2>/dev/null`"
+	if ! [ -n "$SVN_ARCHIVE" ]; then echo "! SVN source code archive is missing"; echo "  expecting 'macsvnserver/ExternalDependencies/Archives/subversion-?.?.?.tar.gz'"; ISERROR=true; fi
+	
+	# were any of the archives missing?
+	if [ "$ISERROR" = "true" ]; then
+		echo ""
+		echo "! some source code archives are missing. please download ExternalDependencies.zip from the MAS website and extract it into the Archives folder"
+		exit 1
+	fi
+	
 	mkdir Sources
 	cd $SOURCES_DIR
 
+	# extract the archives into the Sources directory
+	#!#echo "* extracting $EXPAT_ARCHIVE..."
+	#!#tar -xzf "../$EXPAT_ARCHIVE"
+	echo "* extracting $LIBXML_ARCHIVE..."
+	tar -xzf "../$LIBXML_ARCHIVE"
+	echo "* extracting $GETTEXT_ARCHIVE..."
+	tar -xzf "../$GETTEXT_ARCHIVE"
+	echo "* extracting $NEON_ARCHIVE..."
+	tar -xzf "../$NEON_ARCHIVE"
+	echo "* extracting $APR_ARCHIVE..."
+	tar -xzf "../$APR_ARCHIVE"
+	echo "* extracting $APRUTIL_ARCHIVE..."
+	tar -xzf "../$APRUTIL_ARCHIVE"
+	echo "* extracting $HTTPD_ARCHIVE..."
+	tar -xzf "../$HTTPD_ARCHIVE"
+	echo "* extracting $SVN_ARCHIVE..."
+	tar -xzf "../$SVN_ARCHIVE"
+	
+	echo ""
 	echo "############################################################"
-	echo "# UNPACKING SOURCES"
-
-	HTTPD_ARCHIVE="`find ../Archives -name 'httpd-*.tar.gz' -print -maxdepth 1`"
-	LIBXML_ARCHIVE="`find ../Archives -name 'libxml2-*.tar.gz' -print -maxdepth 1`"
-	EXPAT_ARCHIVE="`find ../Archives -name 'expat-*.tar.gz' -print -maxdepth 1`"
-	SVN_ARCHIVE="`find ../Archives -name 'subversion-*.tar.gz' -print -maxdepth 1`"
-	GETTEXT_ARCHIVE="`find ../Archives -name 'gettext-*.tar.gz' -print -maxdepth 1`"
-
-	echo "$HTTPD_ARCHIVE"
-	tar -xzf "$HTTPD_ARCHIVE"
-	echo "$LIBXML_ARCHIVE"
-	tar -xzf "$LIBXML_ARCHIVE"
-	echo "$EXPAT_ARCHIVE"
-	tar -xzf "$EXPAT_ARCHIVE"
-	echo "$SVN_ARCHIVE"
-	tar -xzf "$SVN_ARCHIVE"
-	echo "$GETTEXT_ARCHIVE"
-	tar -xzf "$GETTEXT_ARCHIVE"
-
-
-	echo "############################################################"
-	echo "# MOVING FOLDERS TO SOURCES FOLDER UNDER STANDARD NAME"
-
-	HTTPD_FOLDER="`find . -name 'httpd-*[^z]' -print -maxdepth 1`"
+	echo "# RENAMING SOURCE FOLDERS TO EXCLUDE VERSION NUMBER"
+	echo ""
+	
+	#!EXPAT_FOLDER="`find . -name 'expat-*[^z]' -print -maxdepth 1`"
 	LIBXML_FOLDER="`find . -name 'libxml2-*[^z]' -print -maxdepth 1`"
-	EXPAT_FOLDER="`find . -name 'expat-*[^z]' -print -maxdepth 1`"
-	SVN_FOLDER="`find . -name 'subversion-*[^z]' -print -maxdepth 1`"
 	GETTEXT_FOLDER="`find . -name 'gettext-*[^z]' -print -maxdepth 1`"
-
-	echo "$HTTPD_FOLDER"
-	mv "$HTTPD_FOLDER" ./httpd
-	echo "$LIBXML_FOLDER"
+	NEON_FOLDER="`find . -name 'neon-*[^z]' -print -maxdepth 1`"
+	APR_FOLDER="`find . -regex '.*/apr-[^u]*' -print -maxdepth 1`"
+	APRUTIL_FOLDER="`find . -name 'apr-util-*[^z]' -print -maxdepth 1`"
+	HTTPD_FOLDER="`find . -name 'httpd-*[^z]' -print -maxdepth 1`"
+	SVN_FOLDER="`find . -name 'subversion-*[^z]' -print -maxdepth 1`"
+	
+	#!#echo "* $EXPAT_FOLDER > ./expat"
+	#!#mv "$EXPAT_FOLDER" ./expat
+	echo "* $LIBXML_FOLDER > ./libxml2"
 	mv "$LIBXML_FOLDER" ./libxml2
-	echo "$EXPAT_FOLDER"
-	mv "$EXPAT_FOLDER" ./expat
-	echo "$SVN_FOLDER"
-	mv "$SVN_FOLDER" ./subversion
-	echo "$GETTEXT_FOLDER"
+	echo "* $GETTEXT_FOLDER > ./gettext"
 	mv "$GETTEXT_FOLDER" ./gettext
-
-
+	echo "* $NEON_FOLDER > ./neon"
+	mv "$NEON_FOLDER" ./neon
+	echo "* $APR_FOLDER > ./apr"
+	mv "$APR_FOLDER" ./apr
+	echo "* $APRUTIL_FOLDER > ./apr-util"
+	mv "$APRUTIL_FOLDER" ./apr-util
+	echo "* $HTTPD_FOLDER > ./httpd"
+	mv "$HTTPD_FOLDER" ./httpd
+	echo "* $SVN_FOLDER > ./subversion"
+	mv "$SVN_FOLDER" ./subversion
+	
+	echo ""
 	echo "############################################################"
 	echo "# CREATING SYMLINKS TO PACKAGES SVN INCLUDES"
-
-	ln -s subversion/apr apr
-	ln -s subversion/apr-util apr-util
-	ln -s subversion/neon neon
+	
+	ln -sv ../apr subversion/apr
+	ln -sv ../apr-util subversion/apr-util
+	ln -sv ../neon subversion/neon
 fi
 
 # the following needs to be before any builds so it doesn't
@@ -81,8 +136,8 @@ echo "############################################################"
 echo "# CLEANING UP SUBVERSION STUFF"
 
 cd subversion
-make clean
-make distclean
+make -s clean
+make -s distclean
 
 echo ""
 echo ""
@@ -96,14 +151,16 @@ export CFLAGS
 #export LDFLAGS
 
 cd ../libxml2
-./configure --prefix=$MAS_PREFIX  --disable-dependency-tracking #&& die "### Couldn't configure libxml2 ###"
+./configure -q --prefix=$MAS_PREFIX  --disable-dependency-tracking #&& die "### Couldn't configure libxml2 ###"
+if [ $? -gt 0 ]; then echo "! configuring of libxml2 failed"; exit 1; fi
 echo "------------------------------------------------------------"
-make clean
-make #&& die "### Couldn't make libxml2 ###"
+make -s clean
+make -s
+if [ $? -gt 0 ]; then echo "! building of libxml2 failed"; exit 1; fi
 echo "------------------------------------------------------------"
-make install #&& die "### Couldn't install libxml2 ###"
+make -s install
+if [ $? -gt 0 ]; then echo "! installing of libxml2 failed"; exit 1; fi
 echo "------------------------------------------------------------"
-
 
 echo ""
 echo ""
@@ -111,11 +168,14 @@ echo "############################################################"
 echo "# BUILDING GETTEXT"
 
 cd ../gettext
-./configure --prefix=$MAS_PREFIX --enable-csharp=no #&& die "### Couldn't configure gettext ###"
+./configure -q --prefix=$MAS_PREFIX --enable-csharp=no
+if [ $? -gt 0 ]; then echo "! configuring of gettext failed"; exit 1; fi
 echo "------------------------------------------------------------"
-make #&& die "### Couldn't make gettext ###"
+make -s
+if [ $? -gt 0 ]; then echo "! building of gettext failed"; exit 1; fi
 echo "------------------------------------------------------------"
-make install #&& die "### Couldn't install gettext ###"
+make -s install
+if [ $? -gt 0 ]; then echo "! installing of gettext failed"; exit 1; fi
 echo "------------------------------------------------------------"
 
 
@@ -125,9 +185,12 @@ echo "############################################################"
 echo "# BUILDING NEON"
 
 cd ../neon
-./configure --prefix=$MAS_PREFIX --with-libs=/Applications/MAS:/usr --with-apxs=${MAS_PREFIX}/bin/apxs #&& die "### Couldn't configure neon ###"
-make #&& die "### Couldn't make neon ###"
-make install #&& die "### Couldn't install neon ###"
+./configure -q --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr --with-apxs=${MAS_PREFIX}/bin/apxs
+if [ $? -gt 0 ]; then echo "! configuring of neon failed"; exit 1; fi
+make -s
+if [ $? -gt 0 ]; then echo "! building of neon failed"; exit 1; fi
+make -s install
+if [ $? -gt 0 ]; then echo "! installing of neon failed"; exit 1; fi
 
 
 echo ""
@@ -136,9 +199,12 @@ echo "############################################################"
 echo "# BUILDING APR"
 
 cd ../apr
-./configure --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr #&& die "### Couldn't configure apr ###"
-make #&& die "### Couldn't make apr ###"
-make install #&& die "### Couldn't install apr ###"
+./configure -q --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr
+if [ $? -gt 0 ]; then echo "! configuring of apr failed"; exit 1; fi
+make -s
+if [ $? -gt 0 ]; then echo "! building of apr failed"; exit 1; fi
+make -s install
+if [ $? -gt 0 ]; then echo "! installing of apr failed"; exit 1; fi
 
 
 echo ""
@@ -147,9 +213,12 @@ echo "############################################################"
 echo "# BUILDING APR-UTIL"
 
 cd ../apr-util
-./configure --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr  --with-apr=$MAS_PREFIX --with-iconv=/usr #&& die "### Couldn't configure apr-util ###"
-make #&& die "### Couldn't make apr-util ###"
-make install #&& die "### Couldn't install apr-util ###"
+./configure -q --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr  --with-apr=$MAS_PREFIX --with-iconv=/usr
+if [ $? -gt 0 ]; then echo "! configuring of apr-util failed"; exit 1; fi
+make -s
+if [ $? -gt 0 ]; then echo "! building of apr-util failed"; exit 1; fi
+make -s install
+if [ $? -gt 0 ]; then echo "! installing of apr-util failed"; exit 1; fi
 
 
 echo ""
@@ -158,11 +227,14 @@ echo "############################################################"
 echo "# BUILDING HTTPD (Apache 2)"
 
 cd ../httpd
-make clean
-make distclean
-./configure --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr --enable-so --enable-dav-fs --enable-dav-lock --enable-dav --enable-static-htpasswd --enable-static-support --disable-authn-dbd --without-sqlite --with-port=8800 #&& die "### Couldn't configure httpd ###"
-make #&& die "### Couldn't make httpd ###"
-make install #&& die "### Couldn't install httpd ###"
+make -s clean
+make -s distclean
+./configure -q --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr --enable-so --enable-dav-fs --enable-dav-lock --enable-dav --enable-static-htpasswd --enable-static-support --disable-authn-dbd --without-sqlite --with-port=8800
+if [ $? -gt 0 ]; then echo "! configuring of httpd failed"; exit 1; fi
+make -s
+if [ $? -gt 0 ]; then echo "! building of httpd failed"; exit 1; fi
+make -s install
+if [ $? -gt 0 ]; then echo "! installing of httpd failed"; exit 1; fi
 
 
 echo ""
@@ -171,9 +243,12 @@ echo "############################################################"
 echo "# BUILDING SUBVERSION"
 
 cd ../subversion
-./configure --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr --with-apr=$MAS_PREFIX --with-apr-util=$MAS_PREFIX --with-apxs=${MAS_PREFIX}/bin/apxs --with-neon=${MAS_PREFIX} #&& die "### Couldn't configure subversion ###"
-make #&& die "### Couldn't make subversion ###"
-make install #&& die "### Couldn't install subversion ###"
+./configure -q --prefix=$MAS_PREFIX --with-libs=${MAS_PREFIX}:/usr --with-apr=$MAS_PREFIX --with-apr-util=$MAS_PREFIX --with-apxs=${MAS_PREFIX}/bin/apxs --with-neon=${MAS_PREFIX}
+if [ $? -gt 0 ]; then echo "! configuring of subversion failed"; exit 1; fi
+make -s
+if [ $? -gt 0 ]; then echo "! building of subversion failed"; exit 1; fi
+make -s install
+if [ $? -gt 0 ]; then echo "! installing of subversion failed"; exit 1; fi
 
 
 echo ""
